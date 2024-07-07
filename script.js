ad5c129d05692ac573314bbfba06bcd57c7e27f7
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-07-07 09:11:00 +0000

    Create findDestinations function that returns the legal moves from specified cell

--- script.js
+++ script.js
@@ -12,23 +12,90 @@ for (let row in rows) {
 }
 console.log(boardArray)
 
-//add node
+
 function addNode(cell) {
     adjancyList.set(cell, [])
 }
 
-// function addEdge(origin, destination) {
-//      adjancyList.get(origin).push(destination);
-//     //  adjancyList.get(destination).push(origin)
-// }
+function addEdge(origin, destination) {
+     adjancyList.get(origin).push(destination);
+    //  adjancyList.get(destination).push(origin)
+}
 
-// for (cell in boardArray) {
-//     console.log(cell)
-//     addNode(String(cell));
-// }
+
 for (let i = 0; i < boardArray.length; i++) {
     addNode(boardArray[i]);
 }
 
 console.log(adjancyList);
+// let destination = function findDestinations(origin) {
+//     let arrayOfDestinations = [];
+//     //rows
+//     if (origin.charAt(0) == 0) {
+//         let possibleRowDestinations = [1, 2];
+//     } else if (origin.charAt(0) == 7) {
+//         let possibleRowDestinations = [5, 6];
+//     } else {
+
+//     }
+    
+// }
+
+// let a, comma, b;
+// for (let origin of adjancyList.keys()) {
+//     // console.log(origin)
+//     // addEdge(origin, destination(origin))
+//     // console.log(origin.charAt())
+
+// }
+
+// for (adjancyList.get(0,0)){
+
+// }
+
+addEdge('0,0','1,3')
+// console.log(adjancyList.get('0,0'));
+
+
+function findDestinations(a,b) {
+    let concatenated = '';
+    let possibleMoves = [[a+2, b+1], [a+1, b+2], [a-1, b+2], [a-2, b+1], [a-2, b-1], [a-1, b-2], [a+1, b-2], [a+2, b-1]];
+    // for (let move in possibleMoves) {
+    //     if (move[0] < 0 || move[0] > 7 || move[1] < 0 || move[1] > 7) {
+            
+    //     }
+    // }
+    // console.log(possibleMoves[0][0], possibleMoves[0][1]);
+
+    console.log(possibleMoves);
+    for (let i = possibleMoves.length - 1; i >= 0; i--) {
+        console.log(possibleMoves[i][0], possibleMoves[i][1]);
+        if (possibleMoves[i][0] < 0 || possibleMoves[i][0] > 7 || possibleMoves[i][1] < 0 || possibleMoves[i][1] > 7) {
+            possibleMoves.splice(i, 1);
+        }
+    }
+    console.log(possibleMoves);
+    // concatenated = concatenated.concat(a, ',', b);
+    // console.log(adjancyList.get(concatenated));
+    
+    // if (a == 0 && b == 0) { //aa == 0 && b == 0
+    //     possibleMoves = [[a+1, b+2], [a+2, b+1]];
+    // } 
+    //a == 0 && b == 0 (a = row nr ; b = column nr)
+    //a == 0 && b == 1
+    //a == 0 && b > 1 && b < 7
+    //a == 0 && b == 7
+    //a == 0 && b == 8
+
+    //a == 1 && b == 0
+    //a == 1 && b == 1
+    //a == 1 && b > 1 && b < 7
+    //a == 1 && b == 7
+    //a == 1 && b == 8
+
+    //5 else ifs with a > 1 && a < 7
+    //5 else ifs with a == 7
+    //5 else ifs with a == 8
+}
+findDestinations(0,0);
 
